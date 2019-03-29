6538743493a7f6c21e647516678b8bb3343c8b81
tabacaruandrei <35452862+tabacaruandrei@users.noreply.github.com>
2019-03-29 07:43:10 +0200

    Add files via upload

---
+++ tabacaruandrei_heap3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int n=5;
+int poz=1;
+int M[100]={35565,533,244,104,87,18};
+
+
+int find_heap(int nume_nod,int heap_size,int value)
+{
+    if(M[nume_nod]==value)
+        {cout<<poz; return 1;}
+    else
+    {
+        if(nume_nod<=heap_size)
+        {
+            poz=poz*10+1;
+        find_heap(nume_nod*2, heap_size, value);
+        poz=(poz/10)*10;
+        find_heap(nume_nod*2+1, heap_size, value);}
+    }
+    return 0;
+}
+
+int main()
+{
+   cout<<"Some huffman code : "<<endl;
+   find_heap(1,n,87);
+}
